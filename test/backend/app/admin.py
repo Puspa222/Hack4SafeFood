10a5cf162850c86ed05e32262647c1d30922fb80
Puspa Regmi <pusparegmi11b@gmail.com>
2025-06-05 09:55:04 +0545

    Remove web frontend components and configuration files

--- test/backend/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import FeedPost, ChatMessage
-
-admin.site.register(FeedPost)
-admin.site.register(ChatMessage)
-# superadmin
--- test/web_frontend/src/components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Logo = () => {
-  return <div className="text-2xl font-bold text-[#2C2C2C]">Hack4SafeFood</div>;
-};
